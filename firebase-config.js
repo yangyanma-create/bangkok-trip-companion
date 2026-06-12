ffa5083b58d4b922af3451f6f081f45652fa43cc
chiyangma <chiyangma@CHIYANGdeMacBook-Pro.local>
2026-06-12 19:48:01 +0800

    Configure Firebase realtime database

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,12 @@
-export const FIREBASE_CONFIG = null;
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBFWZ5LN5DIlc9nMuVEsJgB5mLdjzrITB8",
+  authDomain: "bangkok-trip-companion.firebaseapp.com",
+  databaseURL: "https://bangkok-trip-companion-default-rtdb.firebaseio.com/",
+  projectId: "bangkok-trip-companion",
+  storageBucket: "bangkok-trip-companion.firebasestorage.app",
+  messagingSenderId: "476923465286",
+  appId: "1:476923465286:web:c77c5e066fb48d57280447",
+  measurementId: "G-JQJWYGRYPP",
+};
 
 export const SYNC_ROOM_ID = "bangkok-trip-2026";
